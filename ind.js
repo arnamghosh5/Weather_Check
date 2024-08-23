768e14b63f90c445fe968437ee753b3b4b012273
Arnam Ghosh <88414154+arnamghosh5@users.noreply.github.com>
2024-08-23 23:44:02 +0530

    Add files via upload

---
+++ ind.js
@@ -0,0 +1,31 @@
+async function showWeather(){
+
+	/*setTimeout(()=> {
+		search.value= ""
+	}, 200);*/
+	a=search.value;
+	search.value=""
+	const url = 'https://yahoo-weather5.p.rapidapi.com/weather?location='+a;
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'e10d73ab91mshe853274d4cbf15ap10a917jsn56c214417489',
+		'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result);
+	cityname.innerHTML=a
+	temp.innerHTML="Temparature: " +Math.round(((result.current_observation.condition.temperature)-32)*(5/9))+ " degree C";
+	hum.innerHTML="Humidity: " +result.current_observation.atmosphere.humidity+"%";
+	feel.innerHTML=result.current_observation.condition.text.toString();
+} catch (error) {
+	console.error(error);
+}
+	
+
+
+}
